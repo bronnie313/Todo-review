2a266cbad63f64182436198b248432868be7e298
Ankit <ankit26kushwa@gmail.comm>
2023-05-12 12:03:45 +0530

    Add remove test

--- src/modules/tests/addtask.test.js
+++ src/modules/tests/addtask.test.js
@@ -1,4 +1,4 @@
-import { addTask } from './addAndRemove.js';
+import { addTask, removeTask } from './addAndRemove.js';
 
 describe('addTask', () => {
   test('should add a task to an empty list', () => {
@@ -42,5 +42,69 @@ describe('addTask', () => {
     const result = addTask(task, list);
 
     expect(result).toEqual(expected);
+  });
+});
+
+describe('removeTask', () => {
+  test('should remove a task from a list', () => {
+    const index = 1;
+    const list = [
+      {
+        completed: false,
+        text: 'Task 1',
+        index: 1,
+      },
+      {
+        completed: false,
+        text: 'Task 2',
+        index: 2,
+      },
+      {
+        completed: false,
+        text: 'Task 3',
+        index: 3,
+      },
+    ];
+    const expected = [
+      {
+        completed: false,
+        text: 'Task 1',
+        index: 1,
+      },
+      {
+        completed: false,
+        text: 'Task 3',
+        index: 3,
+      },
+    ];
+
+    const result = removeTask(index, list);
+
+    expect(result).toEqual(expected);
+  });
+
+  test('should not modify the original list', () => {
+    const index = 1;
+    const list = [
+      {
+        completed: false,
+        text: 'Task 1',
+        index: 1,
+      },
+      {
+        completed: false,
+        text: 'Task 2',
+        index: 2,
+      },
+      {
+        completed: false,
+        text: 'Task 3',
+        index: 3,
+      },
+    ];
+
+    const result = removeTask(index, list);
+
+    expect(result).not.toBe(list);
   });
 });
