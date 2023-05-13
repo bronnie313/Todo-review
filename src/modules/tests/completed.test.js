ee3fe6eca52a35117743ae770358247d68893086
Ankit <ankit26kushwa@gmail.comm>
2023-05-13 16:37:22 +0530

    Add Checkstatus

--- src/modules/tests/completed.test.js
+++ src/modules/tests/completed.test.js
@@ -1,4 +1,4 @@
-const { makeTaskEditable, clearCompleted } = require('./completed.js');
+const { makeTaskEditable, clearCompleted, checkStatus } = require('./completed.js');
 
 test('makeTaskEditable updates listName and calls store', () => {
   const taskAreaMock = {
@@ -31,6 +31,29 @@ describe('clearCompleted', () => {
 
     expect(filteredList).toEqual([
       { id: 2, completed: false },
+    ]);
+  });
+});
+
+describe('checkStatus', () => {
+  test('should update completed status based on checkbox state', () => {
+    const checkboxes = [
+      { checked: true },
+      { checked: false },
+      { checked: true },
+    ];
+    const listName = [
+      { completed: false },
+      { completed: true },
+      { completed: false },
+    ];
+
+    const updatedList = checkStatus(checkboxes, listName);
+
+    expect(updatedList).toEqual([
+      { completed: true },
+      { completed: false },
+      { completed: true },
     ]);
   });
 });
