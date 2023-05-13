a68a325747f65716e58a54aaae94004f986de43b
Ankit <ankit26kushwa@gmail.comm>
2023-05-13 16:34:50 +0530

    Add clearCompleted test

--- src/modules/tests/completed.test.js
+++ src/modules/tests/completed.test.js
@@ -1,4 +1,4 @@
-const { makeTaskEditable } = require('./completed.js');
+const { makeTaskEditable, clearCompleted } = require('./completed.js');
 
 test('makeTaskEditable updates listName and calls store', () => {
   const taskAreaMock = {
@@ -17,4 +17,20 @@ test('makeTaskEditable updates listName and calls store', () => {
 
   expect(listName[0].text).toBe('Task 1');
   expect(store).toHaveBeenCalledWith(listName);
+});
+
+describe('clearCompleted', () => {
+  test('should remove completed items from the list', () => {
+    const listName = [
+      { id: 1, completed: true },
+      { id: 2, completed: false },
+      { id: 3, completed: true },
+    ];
+
+    const filteredList = clearCompleted(listName);
+
+    expect(filteredList).toEqual([
+      { id: 2, completed: false },
+    ]);
+  });
 });
